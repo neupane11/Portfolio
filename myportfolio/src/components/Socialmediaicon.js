afc5d7d5f6f2c871d356f036c627c54696976620
neupane11 <sandip.neupane11@gmail.com>
2020-12-23 13:40:55 -0700

    changes made

--- myportfolio/src/components/Socialmediaicon.js
+++ myportfolio/src/components/Socialmediaicon.js
@@ -9,11 +9,11 @@ const  Socialmediaicon=() =>{
         
       <div>
         <ul style={iconStyle}>
-            <li><a href="https://github.com/neupane11"><FaGithub color="bd2c00"/></a></li>
-            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaLinkedin color="0077B5" /></a></li>
-            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaFacebook color="3b5998" /></a></li>
-            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaInstagram color="c13584" /></a></li>
-            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaTwitter color="1da1f2" /></a></li>
+            <li><a href="https://github.com/neupane11"><FaGithub color="white"/></a></li>
+            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaLinkedin color="Linkedin Blue" /></a></li>
+            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaFacebook color="RGB(59, 89, 152)" /></a></li>
+            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaInstagram color="Instagram Blue" /></a></li>
+            <li><a href="https://www.linkedin.com/in/sandip-neupane-56051b17a/"><FaTwitter color="RGB(0, 172, 237)" /></a></li>
             
         </ul>
        
